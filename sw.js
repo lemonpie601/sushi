8c026c69af3a980c3cdc7ce23c491a48dbbd7ef4
lemonpie601 <valorsida@gmail.com>
2026-04-16 10:13:18 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,38 @@
+// 당고집 Service Worker
+// GitHub Pages: https://lemonpie601.github.io/sushi/
+const CACHE = 'dango-v4';
+const ASSETS = [
+  '/sushi/dango.html',
+  '/sushi/manifest.json',
+  '/sushi/icon.svg',
+  '/sushi/icon-192.png',
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+// 네트워크 우선 → 오프라인 시 캐시
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request)
+      .then(res => {
+        const clone = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, clone));
+        return res;
+      })
+      .catch(() => caches.match(e.request))
+  );
+});
